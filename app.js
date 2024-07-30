55c9c52c685fb0ced483520c25e9e78daca5ca8d
fajarot <fajarrahmadyanto@gmail.com>
2024-07-30 14:49:46 +0700

    Update app.js
    Tambah 2 simple endpoint untuk tes jenkins
    
    Signed-off-by: fajarot <fajarrahmadyanto@gmail.com>

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const port = process.env.PORT ?? 3000;
 
 app.use(express.static('public'))
 
+app.get('/', (req, res) => {
+    res.json({message: 'Hallooo'});
+})
+
+app.get('/tes', (res, res) => {
+    res.json({ message: 'Masoookk tesnya', kode: 200});
+});
+
 app.get('*', (req, res) => {
     res.redirect('/');
 })
